b39620d2c217bff67e3dee5d2075adeed8cff38b
sathyatejadvs <daggubatisatyateja@gmail.com>
2019-05-21 17:19:11 +0530

    taxrate file is uploaded

---
+++ taxrate_additional.rb
@@ -0,0 +1,5 @@
+#Calculating the Tax Rates makes easy with Ruby
+amount_total = 10000
+tax_amount = 0.175
+final_amount = amount_total * tax_amount
+puts"The final amount in hand is #{final_amount} with the tax rate as #{tax_amount}"
