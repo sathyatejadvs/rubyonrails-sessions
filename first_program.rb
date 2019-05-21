ecccd0a723e011865d4b706c08a9062bb232e6ef
sathyatejadvs <50663551+sathyatejadvs@users.noreply.github.com>
2019-05-21 14:16:02 +0530

    Add files via upload

---
+++ first_program.rb
@@ -0,0 +1,16 @@
+def withdrawn
+	puts "The amount debited is #{20000+(4500/5)}"
+	puts
+end
+withdrawn
+ 	def swap
+ 		a=100
+ 		b=200
+ 		puts "Before swapping"
+ 		puts a
+ 		puts b
+ 		puts "After Swapping"
+ 		a,b = b,a
+ 		puts a,b 
+ 	end
+ 	swap
